0107e280dfecb1524bdef179f0b96f8d9ee13409
Sameer Shaikh <sameeritis10@gmail.com>
2026-06-30 15:40:54 +0530

    Minor change

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import "dotenv/config";
 
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import connectDB from "./config/db.js";
